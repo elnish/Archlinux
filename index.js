8f58aa296fd0396870f2b726cdbefa19237ae260
elnish <a.shobotova@hotmail.com>
2019-07-15 14:38:59 +0300

    href card

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 //swiper
 var swiper = new Swiper('.swiper-container', {
     scrollbar: {
-    el: '.swiper-scrollbar',
-    hide: true,
+        el: '.swiper-scrollbar',
+        direction: 'horizontal',
+        hide: true,
+    },
+    mousewheel: {
+        invert: true,
     },
 });
 
 window.onload = () => {
     let changeCard = document.querySelectorAll('.main-content__cards-box')[0];
-    let card = changeCard.getElementsByClassName('main-content__card');
-    var priceContainer = document.getElementsByClassName('main-content__price')[0];
+    let card = changeCard.querySelectorAll('.main-content__card');
+    var priceContainer = document.querySelectorAll('.main-content__price')[0];
+    const activeCardClass = 'main-content__card_active';
     price = [8888, 9999, 3333, 5555, 7777, 2222];
     for (let i = 0; i < card.length; i++) {
         card[i].addEventListener('click', function() {
-            let current = changeCard.getElementsByClassName('main-content__card_active');
+            let current = changeCard.querySelectorAll('.main-content__card_active');
             current[0].className = current[0].className.replace(' main-content__card_active', '');
             this.className += ' main-content__card_active';
             priceContainer.textContent = `${price[i]}$`;
@@ -22,7 +27,6 @@ window.onload = () => {
 
     let form = document.querySelector('.main-content__form');
     let input = document.querySelector('.main-content__tel-input');
-    // let re = /^\+(?:[0-9] ?){6,14}[0-9]$/;
     let re = /^\+?([0-9]{2})\)?(\([0-9]{3}\)\s*|[0-9]{3}\-)[0-9]{3}-[0-9]{4}$/;
     function validate(event) {
         event.preventDefault();
